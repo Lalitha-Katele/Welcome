5e03da44e0f525fc67468206674ee13f8dba8ae9
Lalitha <lalitha105105@gmail.com>
2023-12-24 14:21:17 +0530

    First time

---
+++ Demo.java
@@ -0,0 +1,19 @@
+package welcome;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 10 , num2 = 20;
+		System.out.println("Arithametic operation on the two numbers are:");
+		
+		System.out.println("Addition of the numbers is:" + (num1+num2));
+		
+		System.out.println("Subraction of the numbers is:" + (num1-num2));
+		System.out.println("Multiple of the numbers is:" + (num1*num2));
+		System.out.println("Division of the numbers is:" + (num1/num2));
+		
+
+	}
+
+}
